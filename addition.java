83909bcd8a10555bf91165720c67a2c99ff6955a
iancalhy <a01423732@itesm.mx>
2022-03-25 21:11:08 -0600

    add addition base code #1

---
+++ addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+class Suma {
+ 
+   public static void main( String[] args ) {
+      int a, b, c = 0;
+      Scanner s = new Scanner( System.in );
+      System.out.println( "Introduce dos números: ");
+      a = s.nextInt();
+      b = s.nextInt();
+      c = a + b;
+      System.out.println( "La suma de a y b es: " + c );
+   }
+ 
+}
+   
+
+      
+
+
